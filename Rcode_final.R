1be7e9405ee3affcde1472601bde63754c1bbd3a
Shawn Wu <58970305+scwu110927@users.noreply.github.com>
2022-01-13 09:46:02 +0800

    Add files via upload

---
+++ Rcode_final.R
@@ -0,0 +1,34 @@
+# Question3 ####################################################################
+
+set.seed(12345)
+time <- strptime(c("01/01/2020", "12/31/2021"), "%m/%d/%Y")
+shop.time <- sample(seq(from = time[1], to = time[2], by = "day"), 1000, replace = T)
+price <- sample(100:1000, 1000, replace = T)
+subject <- sample(letters[1:20], 1000, replace = T)
+data <- data.frame(shop.time, subject, price)
+
+#(a)
+order.data <- data[order(data$subject), ]
+
+#(b)
+j.last.index <- max(which(order.data$subject == "j"))
+data.a.to.j <- order.data[1:j.last.index, ]
+boxplot(data.a.to.j$price ~ data.a.to.j$subject, xlab = "subject", ylab = "cost")
+
+#(c)
+shop.avg <- NULL
+for (i in 1:20){
+  per.sub.price <- order.data$price[order.data$subject == letters[i]]
+  mean.per.sub.price <- mean(per.sub.price)
+  names(mean.per.sub.price) = letters[i]
+  shop.avg <- c(shop.avg, mean.per.sub.price)
+}
+shop.avg
+
+#(d)
+a.last.index <- max(which(order.data$subject == "a"))
+data.a <- order.data[1:a.last.index, ]
+order.data.a <- data.a[order(data.a$shop.time), ]
+plot(order.data.a$shop.time, order.data.a$price, type = "b", pch = 20, xlab = "time", ylab = "cost")
+
+
